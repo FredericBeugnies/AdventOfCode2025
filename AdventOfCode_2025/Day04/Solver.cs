65970ad28c008d933816884e13e336aad290160b
Frédéric Beugnies <frederic.beugnies.986@infrabel.be>
2025-12-05 12:25:59 +0100

    Day 4 part 2

--- AdventOfCode_2025/Day04/Solver.cs
+++ AdventOfCode_2025/Day04/Solver.cs
@@ -21,7 +21,7 @@
             var grid = ParseInput(inputFilePath);
 
             Console.WriteLine($"Day 4 part 1: {SolvePart1(grid)}");
-            //Console.WriteLine($"Day 4 part 2: {SolvePart2(grid)}");
+            Console.WriteLine($"Day 4 part 2: {SolvePart2(grid)}");
         }
 
         private static Grid ParseInput(string inputFilePath)
@@ -73,6 +73,51 @@
             }
 
             return nbReachableRolls;
+        }
+
+        private static int SolvePart2(Grid grid)
+        {
+            int totalRemovableRolls = 0;
+            int nbReachableRolls = 0;
+
+            do
+            {
+                nbReachableRolls = 0;
+                for (int r = 0; r < grid.NbRows; ++r)
+                {
+                    for (int c = 0; c < grid.NbCols; ++c)
+                    {
+                        if (grid.Cells[r, c] != '@')
+                            continue;
+
+                        int nbAdjacentRolls = 0;
+                        for (int dr = -1; dr <= 1; ++dr)
+                        {
+                            for (int dc = -1; dc <= 1; ++dc)
+                            {
+                                if (dr == 0 && dc == 0)
+                                    continue;
+                                int nr = r + dr;
+                                int nc = c + dc;
+                                if (nr >= 0 && nr < grid.NbRows && nc >= 0 && nc < grid.NbCols)
+                                {
+                                    if (grid.Cells[nr, nc] == '@')
+                                        ++nbAdjacentRolls;
+                                }
+                            }
+                        }
+                        if (nbAdjacentRolls < 4)
+                        {
+                            grid.Cells[r, c] = '.';
+                            ++nbReachableRolls;
+                        }
+                    }
+                }
+                totalRemovableRolls += nbReachableRolls;
+            }
+            while (nbReachableRolls > 0);
+            
+            return totalRemovableRolls;
         }
     }
 }

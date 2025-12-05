abf5ba51b213af9507facbc44485dd8df829dff7
Frédéric Beugnies <frederic.beugnies.986@infrabel.be>
2025-12-05 11:59:00 +0100

    Day 5 part 1

---
+++ AdventOfCode_2025/Day05/Solver.cs
@@ -0,0 +1,56 @@
+﻿namespace AdventOfCode_2025.Day05
+{
+    internal record Range (long Start, long End);
+
+    internal class Solver
+    {
+        internal static void Solve(string inputFilePath)
+        {
+            var (ranges, ingredients) = ParseInput(inputFilePath);
+
+            Console.WriteLine($"Day 5 part 1: {SolvePart1(ranges, ingredients)}");
+            //Console.WriteLine($"Day 5 part 2: {SolvePart2(grid)}");
+        }
+
+        internal static (List<Range>, List<long>) ParseInput(string inputFilePath)
+        {
+            var lines = File.ReadAllLines(inputFilePath);
+
+            int i = 0;
+            var ranges = new List<Range>();
+            while (!string.IsNullOrWhiteSpace(lines[i]))
+            {
+                string[] parts = lines[i].Split('-');
+                ranges.Add(new Range(long.Parse(parts[0]), long.Parse(parts[1])));
+                ++i;
+            }
+
+            ++i; // skip empty line
+            var numbers = new List<long>();
+            while (i < lines.Length)
+            {
+                numbers.Add(long.Parse(lines[i]));
+                ++i;
+            }
+
+            return (ranges, numbers);
+        }
+
+        private static int SolvePart1(List<Range> ranges, List<long> ingredients)
+        {
+            int nbFreshIngredients = 0;
+            foreach (var ingredient in ingredients)
+            {
+                foreach (var range in ranges)
+                {
+                    if (ingredient >= range.Start && ingredient <= range.End)
+                    {
+                        ++nbFreshIngredients;
+                        break;
+                    }
+                }
+            }
+            return nbFreshIngredients;
+        }
+    }
+}
--- AdventOfCode_2025/Program.cs
+++ AdventOfCode_2025/Program.cs
@@ -1,4 +1,5 @@
 ﻿//AdventOfCode_2025.Day01.Solver.Solve("input01.txt");
 //AdventOfCode_2025.Day02.Solver.Solve("input02.txt");
 //AdventOfCode_2025.Day03.Solver.Solve("input03.txt");
-AdventOfCode_2025.Day04.Solver.Solve("input04.txt");
+//AdventOfCode_2025.Day04.Solver.Solve("input04.txt");
+AdventOfCode_2025.Day05.Solver.Solve("input05.txt");

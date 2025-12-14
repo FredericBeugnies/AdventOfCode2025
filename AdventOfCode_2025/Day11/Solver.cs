cc1b70675ec5ac840ea2b9f56ce48c8691065674
Frédéric Beugnies <frederic.beugnies@gmail.com>
2025-12-14 09:41:02 +0100

    Day 11 part 2 + refactoring

--- AdventOfCode_2025/Day11/Solver.cs
+++ AdventOfCode_2025/Day11/Solver.cs
@@ -24,12 +24,32 @@
             }
 
             // solve part 1
-            long[,] nbPaths = new long[nbNodes, nbNodes]; // nbPaths[j, k] = number of paths from start point to node 'j' of length 'k'
-
             int youIdx = nodeIndex["you"];
+            int outIdx = nodeIndex["out"];
+            Console.WriteLine($"Day 11 part 1: {ComputeNbPaths(adjacency, youIdx, outIdx)}");
+
+            // solve part 2
+            int svrIdx = nodeIndex["svr"];
+            int dacIdx = nodeIndex["dac"];
+            int fftIdx = nodeIndex["fft"];
+            long svr_dac = ComputeNbPaths(adjacency, svrIdx, dacIdx);
+            long svr_fft = ComputeNbPaths(adjacency, svrIdx, fftIdx);
+            long dac_fft = ComputeNbPaths(adjacency, dacIdx, fftIdx);
+            long fft_dac = ComputeNbPaths(adjacency, fftIdx, dacIdx);
+            long dac_out = ComputeNbPaths(adjacency, dacIdx, outIdx);
+            long fft_out = ComputeNbPaths(adjacency, fftIdx, outIdx);
+            long res_part2 = svr_dac * dac_fft * fft_out + svr_fft * fft_dac * dac_out;
+            Console.WriteLine($"Day 11 part 2: {res_part2}");
+        }
+
+        private static long ComputeNbPaths(bool[,] adjacency, int from, int to)
+        {
+            int nbNodes = adjacency.GetLength(0);
+            long[,] nbPaths = new long[nbNodes, nbNodes]; // nbPaths[j, k] = number of paths from node 'from' to node 'j' of length 'k'
+
             for (int j = 0; j < nbNodes; ++j)
-                nbPaths[j, 1] = adjacency[youIdx, j] ? 1 : 0;
-            
+                nbPaths[j, 1] = adjacency[from, j] ? 1 : 0;
+
             for (int k = 2; k < nbNodes; ++k)
                 for (int j = 0; j < nbNodes; ++j)
                     for (int m = 0; m < nbNodes; ++m)
@@ -37,10 +57,10 @@
                             nbPaths[j, k] += nbPaths[m, k - 1];
 
             long res = 0;
-            int outIdx = nodeIndex["out"];
             for (int k = 0; k < nbNodes; ++k)
-                res += nbPaths[outIdx, k];
-            Console.WriteLine($"Day 11 part 1: {res}");
+                res += nbPaths[to, k];
+
+            return res;
         }
     }
 }
